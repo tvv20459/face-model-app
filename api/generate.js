c867040eb6615680d9d559480c6066acf7eec2a7
tvv20459 <prompt.code.block.engineer@gmail.com>
2026-02-11 16:25:16 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -38,9 +38,12 @@ export default async function handler(req, res) {
     }
 
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
+  catch (error) {
+  console.error(error);
+
+  return res.status(500).json({
+    error: error.message || "Internal server error"
+  });
 }
+
+
