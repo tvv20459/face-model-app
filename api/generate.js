00ffccc66fe70b8a363464ff4920a160f0205739
tvv20459 <prompt.code.block.engineer@gmail.com>
2026-02-11 16:21:12 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,9 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=
+
+
       {
         method: "POST",
         headers: {
